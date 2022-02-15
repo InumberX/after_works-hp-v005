6cf88f149284206f96d85166827d230a23da92ad
InumberX <35207269+InumberX@users.noreply.github.com>
2022-02-15 09:24:37 +0900

    データベース設定を修正

--- src/app/backend/config/database.js
+++ src/app/backend/config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', 'db'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'after_works_db'),
-      user: env('DATABASE_USERNAME', 'user'),
-      password: env('DATABASE_PASSWORD', 'password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+      port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+      ssl: env.bool("DATABASE_SSL", process.env.DATABASE_SSL),
     },
   },
 });
