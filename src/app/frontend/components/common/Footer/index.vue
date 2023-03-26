c6860a7239345e549aace5c4e449133ce471e8f0
N/NE <35207269+InumberX@users.noreply.github.com>
2023-03-26 16:06:06 +0900

    fix-upgrade nuxt 3 (#24)

---
+++ src/app/frontend/components/common/Footer/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="LayoutFooter"></footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+@use '~/assets/_scss/_global/index' as g;
+</style>
---
+++ src/app/frontend/types/svg.d.ts
@@ -0,0 +1,14 @@
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.svg?inline' {
+  const content: any
+  export default content
+}
+
+declare module '*.svg?component' {
+  const content: any
+  export default content
+}
